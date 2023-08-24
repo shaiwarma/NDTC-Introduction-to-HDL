c90df1aeafd22eaed6d1e9f1a00abfa67cd1d060
shaiwarma <120025425+shaiwarma@users.noreply.github.com>
2023-08-24 11:41:13 +0800

    Add files via upload

---
+++ LaboratoryActivity1/Lab1Part1.vhd
@@ -0,0 +1,12 @@
+LIBRARY ieee; 
+use ieee.std_logic_1164.all;
+
+ENTITY Lab1Part1 IS 
+	PORT (SWITCH: IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			LEDR: OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
+END Lab1Part1;
+
+ARCHITECTURE Behavior OF Lab1Part1 IS 
+BEGIN 
+	LEDR <= SWITCH;
+END Behavior;
